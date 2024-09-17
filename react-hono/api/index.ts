23c40438615f1b27624f6a05985b9cd86c931e96
Bowen7 <zwxdyx@foxmail.com>
2024-09-17 17:20:13 +0800

    vite config

---
+++ react-hono/api/index.ts
@@ -0,0 +1,49 @@
+import { readFile } from 'node:fs/promises'
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+
+const app = new Hono()
+
+class HtmlHandler {
+  static html = ''
+  static async fetch() {
+    if (this.html === '') {
+      if (import.meta.env.PROD) {
+        this.html = await readFile('dist/static/index.html', 'utf8')
+      } else {
+        const html = await readFile('index.html', 'utf8')
+        HtmlHandler.html = html.replace('<head>', `
+          <script type="module">
+      import RefreshRuntime from "/@react-refresh"
+      RefreshRuntime.injectIntoGlobalHook(window)
+      window.$RefreshReg$ = () => {}
+      window.$RefreshSig$ = () => (type) => type
+      window.__vite_plugin_react_preamble_installed__ = true
+      </script>
+      
+          <script type="module" src="/@vite/client"></script>
+          `)
+      }
+    }
+    return HtmlHandler.html
+  }
+}
+
+if (import.meta.env.PROD) {
+  app.use('/assets/*', serveStatic({ root: '/dist/static/' }))
+}
+
+app.get('/', async (c) => {
+  const html = await HtmlHandler.fetch()
+  return c.html(html)
+})
+
+export default app
+
+if (import.meta.env.PROD) {
+  serve({ ...app, port: 3000 }, (info) => {
+    // eslint-disable-next-line no-console
+    console.log(`Listening on http://localhost:${info.port}`)
+  })
+}
--- react-hono/vite.config.ts
+++ react-hono/vite.config.ts
@@ -6,14 +6,37 @@ import build from '@hono/vite-build/node'
 import devServer from '@hono/vite-dev-server'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), build(), devServer({
-    adapter,
-    entry: 'api/index.tsx',
-  })],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  if (mode === 'client') {
+    return {
+      plugins: [react()],
+      build: {
+        rollupOptions: {
+          input: './index.html',
+          output: {
+            dir: './dist/static',
+          },
+        },
+        copyPublicDir: false,
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        },
+      },
+    }
+  }
+  return ({
+    plugins: [build({
+      entry: ['./api/index.ts'],
+    }), devServer({
+      adapter,
+      entry: 'api/index.ts',
+    }), react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+  })
 })
